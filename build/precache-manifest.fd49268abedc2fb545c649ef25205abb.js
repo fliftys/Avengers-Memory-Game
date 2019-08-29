814ba7595a3de9eb49c009f7fcb549ed3038564b
fliftys <fliftys@gmail.com>
2019-08-29 07:27:41 +0200

    Add build version to netlify

---
+++ build/precache-manifest.fd49268abedc2fb545c649ef25205abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0944ec7c8bd90429ac67ef88446feac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d1a4fa295a51a8dfe31",
+    "url": "/static/css/main.61604722.chunk.css"
+  },
+  {
+    "revision": "2221acd0946998d322fe",
+    "url": "/static/js/2.86fa7ce0.chunk.js"
+  },
+  {
+    "revision": "7d1a4fa295a51a8dfe31",
+    "url": "/static/js/main.d1aa27c9.chunk.js"
+  },
+  {
+    "revision": "58db94c48b77e174b772",
+    "url": "/static/js/runtime~main.318be517.js"
+  },
+  {
+    "revision": "5f2d9065e1c51f6e873db72bd3d7d52d",
+    "url": "/static/media/logo.5f2d9065.jpg"
+  }
+]);
